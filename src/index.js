0749ab7dbc2318be9ffc11c79a148f7ecf32ee3f
NikolayP <nikipanovski@gmail.com>
2025-09-05 00:58:43 +0300

    Add scrolling to top on new pages

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import ScrollToTop from 'components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>
